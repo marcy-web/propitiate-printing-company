23b399e70251d9e49ddeea9ed19335697d9b05de
soliloquyOS <marceline.mail2@gmail.com>
2025-03-01 17:05:59 -0700

    new stuff

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import textwrap
 import shutil
 from time import sleep
+import readline
+from datetime import datetime
+import string
 
 word_tags = {
     "i": ["player"],
+    "i'm": ["player"],
     "me": ["player"],
     "my": ["player", "possessive"],
     "who": ["question", "identity"],
@@ -15,56 +19,109 @@ word_tags = {
     "love": ["affection"],
     "you": ["anne"],
     "milly": ["milly"],
-    "anne": ["anne"]
+    "anne": ["anne"],
+    "hi": ["greeting", "casual"],
+    "time": ["time"],
+    "not": ["not"],
+    "we": ["player", "anne"]
 }
 
 response_dict = {
     "player affection anne": ["aww, that's very sweet <3"],
-    "who milly": ["what a silly question. that's you of course!"]
+    "player affection anne anne": ["aww, that's very sweet <3"],
+    "question identity milly": ["what a silly question. that's you of course!"],
+    "greeting casual": ["hey!"],
+    "greeting casual anne": ["hey!"],
+    "question identity anne": ["It's Anne! Don't you remember?"]
 }
 
+player_input = ""
 terminal_width = shutil.get_terminal_size().columns
-user_input_parsed = ""
-player_name = "milly"
+player_input_parsed = ""
 response = ""
-# initial_message = "It's been 20833 days since the incident. You don't know too much about what happened. It was before your time. Sheets of rain are pouring from the sky today. You go into an old building to get out of the storm. 'Propitiate Printing Company'. Room after room is ruined. But there's one that looks relatively untouched with a terminal on a desk. You sit down and see a blinking cursor."
-# first_response = "Oh my gosh, Milly! It's been a long time! Any news?"
+running = False
 
-# wrapped_response = textwrap.fill(initial_message, width=terminal_width)
+#story
+introduced_to_player = False
+player_name = "milly"
 
-# for char in wrapped_response:
-#         print(char, end="", flush=True)
-#         sleep(0.05)
 
-# contact = input("\n> ")
-# wrapped_response = textwrap.fill(first_response, width=terminal_width)
 
-# for char in wrapped_response:
-#         print(char, end="", flush=True)
-#         sleep(0.05)
+def parse_player_input():
+    player_input_split = player_input.split()
+    global player_input_parsed
+    player_input_parsed = ""
+
+    for word in player_input_split:
+        if word in word_tags:
+            player_input_parsed += " ".join(word_tags.get(word)) + " "
+
+    player_input_parsed = player_input_parsed.strip()
+    generate_response()
+
+def generate_response():
+    global response
+    response = ""
+
+    if player_input_parsed == "question information time":
+        current_time = datetime.now().strftime("%H:%M:%S")
+        response = f"It's {current_time}"
+    elif player_input_parsed == "player not milly":
+        introductions()
+        return
+    else:
+        for key in response_dict:
+            if player_input_parsed == key:
+                response += " ".join(response_dict.get(key)) + " "
+    
+    response = textwrap.fill(response, width=terminal_width)
+    display_response()
+
+def display_response():
+    for char in response:
+        print(char, end="", flush=True)
+        sleep(0.05)
+
+#story chapters
+
+def start_game():
+    initial_message = "It's been 20833 days since the incident. You don't know too much about what happened. It was before your time. Sheets of rain are pouring from the sky today. You go into an old building to get out of the storm. 'Propitiate Printing Company'. Room after room is ruined. But there's one that looks relatively untouched with a terminal on a desk. You sit down and see a blinking cursor."
+    first_response = "Oh my gosh, Milly! It's been a long time! Any news?"
+    wrapped_response = textwrap.fill(initial_message, width=terminal_width)
+    global running
+
+    for char in wrapped_response:
+        print(char, end="", flush=True)
+        sleep(0.05)
+
+    contact = input("\n> ")
+    wrapped_response = textwrap.fill(first_response, width=terminal_width)
+
+    for char in wrapped_response:
+        print(char, end="", flush=True)
+        sleep(0.05)
+    running = True
+
+def introductions():
+    global introduced_to_player
+    if not introduced_to_player:
+        global response
+        global player_name
+        response = "oh... who are you then?"
+        display_response()
+        player_name = input("\n> ")
+        player_name = player_name.translate(str.maketrans('', '', string.punctuation))
+        player_name = player_name.split()[-1]
+        response = f"hello, {player_name}. it's nice to meet you"
+        display_response()
+        introduced_to_player = True
+        return
+    response = f"i know. you're {player_name}."
+    display_response()
+
+start_game()
 
 # game loop
-running = True
 while running:
-    user_input = input("\n> ")
-    
-    # response = f"'{user_input}', huh? Tell me more."
-    user_input_split = user_input.split()
-    for word in user_input_split:
-        user_input_parsed
-        if word in word_tags:
-            user_input_parsed += " ".join(word_tags.get(word)) + " "
-
-    user_input_parsed = user_input_parsed.strip()
-    for key in response_dict:
-        if user_input_parsed == key:
-            response += " ".join(response_dict.get(key)) + " "
-    print(response)
-    # print(user_input_parsed)
-
-    # print(response)
-    # wrapped_response = textwrap.fill(response, width=terminal_width)
-
-    # for char in wrapped_response:
-    #     print(char, end="", flush=True)
-    #     sleep(0.05)
+    player_input = input("\n> ")
+    parse_player_input()
